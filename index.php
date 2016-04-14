935cdc2b9b9dda2500287c7f68d609b1338d445f
ramc256 <rosangela.ing10@gmail.com>
2016-04-13 20:57:36 -0430

    subir el primero cambio a la web lcm
    icm1

---
+++ index.php
@@ -0,0 +1,99 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="styles/styles.css">
+</head>
+<body>  
+	<div class="video">
+		<div class="video-texture active_texture" style="opacity: 1;"></div>
+		<iframe id="tubular-player" style="position: absolute; height: 759px; width: 1348.5px; left: 0px; top: 0px;" frameborder="0" allowfullscreen="1" title="YouTube video player" width="1349" height="759" src="https://www.youtube.com/embed/5uBSQOn1Sx8?&autoplay=1&loop=1&playlist=5uBSQOn1Sx8"></iframe>
+		<div class="header">
+			<ul>
+	 			<li><a href="#home">Home</a></li>
+	 			<li><a href="#news">News</a></li>
+	  			<li><a href="#contact">Contact</a></li>
+	  			<li><a href="#about">About</a></li>
+			</ul>
+		</div>
+	</div>
+	<div class="logo-lcm">
+		<img src="images/logo.png"  WIDTH="878" HEIGHT="380">
+	</div>
+	<h1 class="titulo">Nuestro Productos</h1>
+	<div class="row">
+		<div class="bloque1" data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Impresión POP</h2>
+			<div class="line"></div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla a ex finibus euismod. Aenean in iaculis dolor. Maecenas ullamcorper nisl sed vehicula ullamcorper. Etiam eu gravida dolor. Nunc congue sed nisi et fermentum. Aenean eleifend erat facilisis nisi tristique vehicula.</p>
+		</div>
+		<div class="bloque2"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Ferias</h2>
+			<div class="line"></div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla a ex finibus euismod. Aenean in iaculis dolor. Maecenas ullamcorper nisl sed vehicula ullamcorper. Etiam eu gravida dolor. Nunc congue sed nisi et fermentum. Aenean eleifend erat facilisis nisi tristique vehicula.</p>
+		</div>
+		<div class="bloque3"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Marketing</h2>
+			<div class="line"></div>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla a ex finibus euismod. Aenean in iaculis dolor. Maecenas ullamcorper nisl sed vehicula ullamcorper. Etiam eu gravida dolor. Nunc congue sed nisi et fermentum. Aenean eleifend erat facilisis nisi tristique vehicula.</p>
+		</div>
+	</div>
+	<div class="row2"> 
+		<h2>Nuestros productos</h2>
+		<img class="row-image" src="images/1.jpg">
+		<img class="row-image" src="images/1.jpg">
+		<img class="row-image" src="images/1.jpg">
+	</div>
+	<h1 class="titulo1">Team</h1>
+	<div class="row3">
+		<div class="bloque1" data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Diseñador</h2>
+			<div class="line"></div>	
+		</div>
+		<div class="bloque2"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Periodista</h2>
+			<div class="line"></div>
+		</div>
+		<div class="bloque3"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Ingeniero</h2>
+			<div class="line"></div>
+		</div>
+		<div class="bloque4"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Ingeniero</h2>
+			<div class="line"></div>
+		</div>
+		<div class="bloque5"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Ingeniero</h2>
+			<div class="line"></div>
+		</div>
+		<div class="bloque6"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+			<div class="cir"></div>
+			<h2>Ingeniero</h2>
+			<div class="line"></div>
+		</div>
+	</div>
+	<div class="row4"> 		
+		<div class="fondo-green">
+			<img class="img-abouts" src="images/2.jpg">
+		</div>
+		<div class="bloques">
+			<div class="bloque7" > CONTENIDO</div>
+			<div class="bloque8" > CONTENIDO</div>
+			<div class="bloque9" > CONTENIDO</div>
+			<div class="bloque10" > CONTENIDO</div>
+			<div class="bloque11" > CONTENIDO</div>
+			<div class="bloque12" > CONTENIDO</div>
+		</div>
+	</div>
+	<div class="footer"> 
+		<p class="footer">© Latin Canadian Media</p>
+	</div>
+</body>
+</html>
