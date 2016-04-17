6f25240d6d891a786a941d054fa1223374ddcf72
ramc256 <rosangela.ing10@gmail.com>
2016-04-17 00:01:58 -0430

    cambios css
    cambios css

--- index.php
+++ index.php
@@ -39,61 +39,113 @@
 			<div class="line"></div>
 			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id nulla a ex finibus euismod. Aenean in iaculis dolor. Maecenas ullamcorper nisl sed vehicula ullamcorper. Etiam eu gravida dolor. Nunc congue sed nisi et fermentum. Aenean eleifend erat facilisis nisi tristique vehicula.</p>
 		</div>
+		<div class="clear"></div>
 	</div>
 	<div class="row2"> 
-		<h2>Nuestros productos</h2>
+		<h1>Nuestros productos</h1>
 		<img class="row-image" src="images/1.jpg">
 		<img class="row-image" src="images/1.jpg">
 		<img class="row-image" src="images/1.jpg">
+		<div class="clear"></div>
 	</div>
 	<h1 class="titulo1">Team</h1>
 	<div class="row3">
-		<div class="bloque1" data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque1 bloque-seccion" data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Diseñador</h2>
 			<div class="line"></div>	
 		</div>
-		<div class="bloque2"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque2 bloque-seccion"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Periodista</h2>
 			<div class="line"></div>
 		</div>
-		<div class="bloque3"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque3 bloque-seccion"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Ingeniero</h2>
 			<div class="line"></div>
 		</div>
-		<div class="bloque4"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque4 bloque-seccion"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Ingeniero</h2>
 			<div class="line"></div>
 		</div>
-		<div class="bloque5"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque5 bloque-seccion"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Ingeniero</h2>
 			<div class="line"></div>
 		</div>
-		<div class="bloque6"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
+		<div class="bloque6 bloque-seccion"  data-scroll-reveal="enter left after 0.15s over 1s" data-sr-init="true" data-sr-complete="true">
 			<div class="cir"></div>
 			<h2>Ingeniero</h2>
 			<div class="line"></div>
 		</div>
+		<div class="clear"></div>
 	</div>
 	<div class="row4"> 		
 		<div class="fondo-green">
 			<img class="img-abouts" src="images/2.jpg">
 		</div>
 		<div class="bloques">
-			<div class="bloque7" > CONTENIDO</div>
-			<div class="bloque8" > CONTENIDO</div>
-			<div class="bloque9" > CONTENIDO</div>
-			<div class="bloque10" > CONTENIDO</div>
-			<div class="bloque11" > CONTENIDO</div>
-			<div class="bloque12" > CONTENIDO</div>
+			<div class="bloque7 bloque-text" >
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+					Nam facilisis nulla fermentum eros gravida, dictum lacinia arcu pulvinar.
+					Praesent placerat euismod elit, ac vestibulum mauris tincidunt a.
+					Suspendisse sagittis, nunc sed accumsan malesuada, tortor massa suscipit neque, sed posuere leo ante nec augue.
+
+				</p> 
+			</div>
+			<div class="bloque8 bloque-text" >
+				<p>
+					Vivamus quis molestie mauris, ac mollis justo.
+					Nam eget eros tincidunt, convallis magna a, elementum tortor.
+					Fusce lacinia nunc ornare libero pellentesque, a rutrum orci posuere. 
+					Nam auctor, ligula a facilisis vestibulum, dui nisl varius tortor, sit amet condimentum quam neque laoreet lorem.
+					Mauris ac ex arcu. Sed sit amet felis erat. Vestibulum et fermentum nisl.
+				</p>
+			</div>
+			<div class="bloque9 bloque-text" >
+				<p>
+					Nulla vitae augue nec ante dapibus malesuada.
+					Vestibulum eleifend facilisis nisl, et pulvinar ligula iaculis sit amet.
+					Fusce eleifend condimentum erat sit amet sagittis. Nulla luctus vehicula lorem.
+					Donec sit amet nulla eu justo lobortis gravida non eu tortor. 
+					Aenean nec nisi ac neque scelerisque facilisis.
+				</p>
+			</div>
+			<div class="bloque10 bloque-text" > 
+				<p>
+					Vestibulum ut lorem malesuada, vestibulum dolor sed, dapibus eros.
+					Sed viverra purus consectetur velit molestie, non cursus tortor mollis.
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Vestibulum mollis, sem vel blandit mattis, elit arcu ultrices justo, ac ultrices velit velit quis leo.
+					Vivamus convallis suscipit dui, quis porta metus fringilla nec.
+					Integer accumsan leo congue urna facilisis dictum.
+				</p>
+			</div>
+			<div class="bloque11 bloque-text" > 
+				<p>
+					Cras finibus, dolor nec pulvinar tincidunt, nunc quam commodo justo, id rhoncus eros orci sed ligula.
+					Pellentesque cursus convallis condimentum. Aliquam fermentum viverra tortor a laoreet.
+					Integer blandit venenatis lacinia. Proin sit amet consequat massa.
+					Donec et lectus non purus pharetra porttitor. Morbi aliquet cursus nibh.
+				</p>
+			</div>
+			<div class="bloque12 bloque-text" >
+				<p>
+					Fusce massa turpis, congue et nulla at, ultrices tempor dolor.
+					Integer nec imperdiet lacus. Suspendisse elementum et dolor eget hendrerit.
+					Phasellus tristique posuere elit, elementum congue diam interdum in. 
+					Suspendisse vestibulum auctor velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra,
+				</p>
+			</div>
+			<div class="clear"></div>
 		</div>
 	</div>
-	<div class="footer"> 
-		<p class="footer">© Latin Canadian Media</p>
-	</div>
+
+	<footer> 
+		<p>© Latin Canadian Media</p>
+	</footer>
 </body>
 </html>
